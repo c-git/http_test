e1cdb39b49449cb2fca254bf2a3510199e3eb78e
One <43485962+c-git@users.noreply.github.com>
2024-12-31 23:15:37 -0500

    feat: add support for customizing UI

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,38 @@
+mod ui_page;
+
+use log::info;
+use ui_page::UiPage;
+
 const HEADING: &str = concat!("Test App ver: ", env!("CARGO_PKG_VERSION"));
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize, Default)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TestApp {}
+pub struct TestApp {
+    ui_page: Option<UiPage>,
+}
 
 impl TestApp {
+    const VISUALS_KEY: &str = "visuals";
+
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        if let Some(visuals) = cc
+            .egui_ctx
+            .data_mut(|r| r.get_persisted::<egui::Visuals>(egui::Id::new(Self::VISUALS_KEY)))
+        {
+            info!("Found saved Visuals. Loading...");
+            cc.egui_ctx.set_visuals(visuals);
+        } else {
+            info!("Unable to load Visuals, no saved version found");
+        }
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
+            info!("Attempting to load previous app state");
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
         } else {
             Default::default()
@@ -24,6 +43,7 @@ impl TestApp {
 impl eframe::App for TestApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        info!("Saving app state");
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
@@ -32,20 +52,29 @@ impl eframe::App for TestApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
+        if let Some(ui_page) = self.ui_page.as_mut() {
+            match ui_page.show(ctx.clone()) {
+                ui_page::ShouldClose::Close => self.ui_page = None,
+                ui_page::ShouldClose::LeaveOpen => {}
+            };
+        }
+
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
             egui::menu::bar(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
+                ui.menu_button("File", |ui| {
+                    if ui.button("Open UI Settings").clicked() {
+                        self.ui_page = Some(Default::default());
+                        ui.close_menu();
+                    }
+                    if !is_web && ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+                ui.add_space(16.0);
 
                 egui::widgets::global_theme_preference_buttons(ui);
             });
---
+++ src/app/ui_page.rs
@@ -0,0 +1,49 @@
+use log::info;
+
+use crate::TestApp;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Default)]
+#[serde(default)]
+pub struct UiPage {
+    #[serde(skip)]
+    prev_ui_options: Option<egui::Options>,
+}
+
+pub enum ShouldClose {
+    Close,
+    LeaveOpen,
+}
+
+impl UiPage {
+    fn save_current_ui_options(&mut self, ctx: egui::Context) {
+        let current_ui_options = ctx.options(|o| o.clone());
+        self.prev_ui_options = Some(current_ui_options);
+        let visuals = ctx.style().visuals.clone();
+        ctx.data_mut(|w| w.insert_persisted(egui::Id::new(TestApp::VISUALS_KEY), visuals));
+        info!("Saved UI Visuals");
+    }
+
+    pub fn show(&mut self, ctx: egui::Context) -> ShouldClose {
+        let mut is_open = true;
+        egui::Window::new("UI Settings")
+            .vscroll(true)
+            .hscroll(true)
+            .open(&mut is_open)
+            .show(&ctx.clone(), |ui| {
+                ctx.settings_ui(ui);
+                match self.prev_ui_options.as_ref() {
+                    Some(prev) => {
+                        if ctx.options(|o| o != prev) {
+                            self.save_current_ui_options(ctx)
+                        }
+                    }
+                    None => self.save_current_ui_options(ctx),
+                }
+            });
+        if is_open {
+            ShouldClose::LeaveOpen
+        } else {
+            ShouldClose::Close
+        }
+    }
+}
