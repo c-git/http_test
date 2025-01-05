f5997bfe7055749eb5afc3fd6a5de678fba417cf
One <43485962+c-git@users.noreply.github.com>
2025-01-05 04:51:16 -0500

    fix: clean up WASM deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #![warn(clippy::all, rust_2018_idioms, unused_crate_dependencies)]
 
 #[cfg(not(target_arch = "wasm32"))]
-mod suppress_used_in_bin {
+mod suppress_used_in_native_bin {
     use tokio as _;
     use tracing_subscriber as _;
+}
+#[cfg(target_arch = "wasm32")]
+mod suppress_used_in_wasm_bin {
+    use log as _;
+    use wasm_bindgen_futures as _;
+    use web_sys as _;
 }
 
 mod app;
