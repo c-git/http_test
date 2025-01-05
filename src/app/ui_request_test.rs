1ce4f5767e481a5ae434f7b64ead8add3707f06a
One <43485962+c-git@users.noreply.github.com>
2025-01-05 00:56:27 -0500

    feat: switch over to reqwest-cross
    Get the basics working

--- src/app/ui_request_test.rs
+++ src/app/ui_request_test.rs
@@ -1,47 +1,11 @@
-//! Based on https://www.egui.rs/#http
+//! Original structure taken from https://www.egui.rs/#http
 
-use egui::Image;
-use poll_promise::Promise;
+use anyhow::Context as _;
+use reqwest_cross::{fetch_plus, reqwest, Awaiting, DataState};
 
-struct Resource {
-    /// HTTP response
-    response: ehttp::Response,
-
-    text: Option<String>,
-
-    /// If set, the response was an image.
-    image: Option<Image<'static>>,
-
-    /// If set, the response was text with some supported syntax highlighting (e.g. ".rs" or ".md").
-    colored_text: Option<ColoredText>,
-}
-
-impl Resource {
-    fn from_response(ctx: &egui::Context, response: ehttp::Response) -> Self {
-        let content_type = response.content_type().unwrap_or_default();
-        if content_type.starts_with("image/") {
-            ctx.include_bytes(response.url.clone(), response.bytes.clone());
-            let image = Image::from_uri(response.url.clone());
-
-            Self {
-                response,
-                text: None,
-                colored_text: None,
-                image: Some(image),
-            }
-        } else {
-            let text = response.text();
-            let colored_text = text.and_then(|text| syntax_highlighting(ctx, &response, text));
-            let text = text.map(|text| text.to_owned());
-
-            Self {
-                response,
-                text,
-                colored_text,
-                image: None,
-            }
-        }
-    }
+struct ResponseData {
+    url: String,
+    text: String,
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -49,191 +13,161 @@ pub struct UiRequestTest {
     url: String,
 
     #[serde(skip)]
-    promise: Option<Promise<ehttp::Result<Resource>>>,
+    client: reqwest::Client,
+
+    #[serde(skip)]
+    /// When Option::None we don't want to load anything otherwise try to load current url
+    resp_data: Option<DataState<ResponseData>>,
 }
 
 impl Default for UiRequestTest {
     fn default() -> Self {
         Self {
-            url: "https://raw.githubusercontent.com/emilk/egui/master/README.md".to_owned(),
-            promise: Default::default(),
+            url: "https://github.com/c-git/reqwest_w_egui_testing/blob/main/README.md".to_owned(),
+            resp_data: Default::default(),
+            client: reqwest::Client::builder()
+                .cookie_store(true)
+                .build()
+                .expect("failed to create reqwest client"),
         }
     }
 }
 
 impl UiRequestTest {
     pub fn show(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
-        let prev_url = self.url.clone();
-        let trigger_fetch = ui_url(ui, frame, &mut self.url);
+        self.ui_url(ui, frame);
 
         ui.horizontal_wrapped(|ui| {
             ui.spacing_mut().item_spacing.x = 0.0;
             ui.label("HTTP requests made using ");
-            ui.hyperlink_to("ehttp", "https://www.github.com/emilk/ehttp");
+            ui.hyperlink_to("reqwest-cross", "https://github.com/c-git/reqwest-cross");
             ui.label(".");
         });
 
-        if trigger_fetch {
-            let ctx = ui.ctx().clone();
-            let (sender, promise) = Promise::new();
-            let request = ehttp::Request::get(&self.url);
-            ehttp::fetch(request, move |response| {
-                ctx.forget_image(&prev_url);
-                ctx.request_repaint(); // wake up UI thread
-                let resource = response.map(|response| Resource::from_response(&ctx, response));
-                sender.send(resource);
-            });
-            self.promise = Some(promise);
-        }
-
         ui.separator();
 
-        if let Some(promise) = &self.promise {
-            if let Some(result) = promise.ready() {
-                match result {
-                    Ok(resource) => {
-                        ui_resource(ui, resource);
-                    }
-                    Err(error) => {
-                        // This should only happen if the fetch API isn't available or something similar.
-                        ui.colored_label(
-                            ui.visuals().error_fg_color,
-                            if error.is_empty() { "Error" } else { error },
-                        );
-                    }
-                }
+        if let Some(resp_data) = self.resp_data.as_mut() {
+            if let DataState::Present(resp) = resp_data {
+                ui_resource(ui, resp);
             } else {
-                ui.spinner();
+                let ctx = ui.ctx().clone();
+                let url = self.url.clone();
+                resp_data.get(|| {
+                    let req = self.client.get(&self.url);
+                    let response_handler = |resp: reqwest::Result<reqwest::Response>| async {
+                        let text = resp
+                            .context("failed to get response, request failed")?
+                            .text()
+                            .await
+                            .context("failed to get text from response")?;
+                        Ok(ResponseData { url, text })
+                    };
+                    let ui_notify = move || {
+                        ctx.request_repaint();
+                    };
+                    let rx = fetch_plus(req, response_handler, ui_notify);
+                    Awaiting(rx)
+                });
             }
         }
     }
-}
 
-fn ui_url(ui: &mut egui::Ui, frame: &eframe::Frame, url: &mut String) -> bool {
-    let mut trigger_fetch = false;
+    fn ui_url(&mut self, ui: &mut egui::Ui, frame: &eframe::Frame) {
+        ui.horizontal(|ui| {
+            ui.label("URL:");
+            if ui
+                .add(egui::TextEdit::singleline(&mut self.url).desired_width(f32::INFINITY))
+                .lost_focus()
+            {
+                self.trigger_fetch();
+            }
+            // TODO 2: Add a submit button (maybe use a right to left layout and put the button first)
+        });
 
-    ui.horizontal(|ui| {
-        ui.label("URL:");
-        trigger_fetch |= ui
-            .add(egui::TextEdit::singleline(url).desired_width(f32::INFINITY))
-            .lost_focus();
-    });
-
-    // TODO 2: Add a submit button
-
-    if frame.is_web() {
-        ui.label("HINT: paste the url of this page into the field above!");
+        if frame.is_web() {
+            ui.label("HINT: paste the url of this page into the field above!");
+        }
     }
 
-    ui.horizontal(|ui| {
-        if ui.button("Source code for this example").clicked() {
-            *url = format!(
-                "https://raw.githubusercontent.com/emilk/egui/master/{}",
-                file!()
-            );
-            trigger_fetch = true;
-        }
-        if ui.button("Random image").clicked() {
-            let seed = ui.input(|i| i.time);
-            let side = 640;
-            *url = format!("https://picsum.photos/seed/{seed}/{side}");
-            trigger_fetch = true;
-        }
-    });
-
-    trigger_fetch
+    /// Idempotent as long in all practical respects, unless the fetch already started, but then what else should we do
+    fn trigger_fetch(&mut self) {
+        self.resp_data = Some(Default::default()); // Clear any current data and set to some to activate the loading and polling
+    }
 }
 
-fn ui_resource(ui: &mut egui::Ui, resource: &Resource) {
-    let Resource {
-        response,
-        text,
-        image,
-        colored_text,
-    } = resource;
-
-    ui.monospace(format!("url:          {}", response.url));
-    ui.monospace(format!(
-        "status:       {} ({})",
-        response.status, response.status_text
-    ));
-    ui.monospace(format!(
-        "content-type: {}",
-        response.content_type().unwrap_or_default()
-    ));
-    ui.monospace(format!(
-        "size:         {:.1} kB",
-        response.bytes.len() as f32 / 1000.0
-    ));
+fn ui_resource(ui: &mut egui::Ui, resp: &ResponseData) {
+    ui.monospace(format!("url:          {}", resp.url));
+    // ui.monospace(format!(
+    //     "status:       {} ({})",
+    //     response.status, response.status_text
+    // ));
+    // ui.monospace(format!(
+    //     "content-type: {}",
+    //     response.content_type().unwrap_or_default()
+    // ));
+    // ui.monospace(format!(
+    //     "size:         {:.1} kB",
+    //     response.bytes.len() as f32 / 1000.0
+    // ));
 
     ui.separator();
 
     egui::ScrollArea::vertical()
         .auto_shrink(false)
         .show(ui, |ui| {
-            egui::CollapsingHeader::new("Response headers")
-                .default_open(false)
-                .show(ui, |ui| {
-                    egui::Grid::new("response_headers")
-                        .spacing(egui::vec2(ui.spacing().item_spacing.x * 2.0, 0.0))
-                        .show(ui, |ui| {
-                            for (k, v) in &response.headers {
-                                ui.label(k);
-                                ui.label(v);
-                                ui.end_row();
-                            }
-                        })
-                });
+            // egui::CollapsingHeader::new("Response headers")
+            //     .default_open(false)
+            //     .show(ui, |ui| {
+            //         egui::Grid::new("response_headers")
+            //             .spacing(egui::vec2(ui.spacing().item_spacing.x * 2.0, 0.0))
+            //             .show(ui, |ui| {
+            //                 for (k, v) in &response.headers {
+            //                     ui.label(k);
+            //                     ui.label(v);
+            //                     ui.end_row();
+            //                 }
+            //             })
+            //     });
 
+            // ui.separator();
+
+            let tooltip = "Click to copy the response body";
+            if ui.button("📋").on_hover_text(tooltip).clicked() {
+                ui.ctx().copy_text(resp.text.clone());
+            }
             ui.separator();
 
-            if let Some(text) = &text {
-                let tooltip = "Click to copy the response body";
-                if ui.button("📋").on_hover_text(tooltip).clicked() {
-                    ui.ctx().copy_text(text.clone());
-                }
-                ui.separator();
-            }
-
-            if let Some(image) = image {
-                ui.add(image.clone());
-            } else if let Some(colored_text) = colored_text {
-                colored_text.ui(ui);
-            } else if let Some(text) = &text {
-                ui.add(egui::Label::new(text).selectable(true));
-            } else {
-                ui.monospace("[binary]");
-            }
+            ui.add(egui::Label::new(&resp.text).selectable(true));
         });
 }
 
 // ----------------------------------------------------------------------------
 // Syntax highlighting:
 
-fn syntax_highlighting(
-    ctx: &egui::Context,
-    response: &ehttp::Response,
-    text: &str,
-) -> Option<ColoredText> {
-    let extension_and_rest: Vec<&str> = response.url.rsplitn(2, '.').collect();
-    let extension = extension_and_rest.first()?;
-    let theme = egui_extras::syntax_highlighting::CodeTheme::from_style(&ctx.style());
-    Some(ColoredText(egui_extras::syntax_highlighting::highlight(
-        ctx,
-        &ctx.style(),
-        &theme,
-        text,
-        extension,
-    )))
-}
+// fn syntax_highlighting(
+//     ctx: &egui::Context,
+//     response: &ehttp::Response,
+//     text: &str,
+// ) -> Option<ColoredText> {
+//     let extension_and_rest: Vec<&str> = response.url.rsplitn(2, '.').collect();
+//     let extension = extension_and_rest.first()?;
+//     let theme = egui_extras::syntax_highlighting::CodeTheme::from_style(&ctx.style());
+//     Some(ColoredText(egui_extras::syntax_highlighting::highlight(
+//         ctx,
+//         &ctx.style(),
+//         &theme,
+//         text,
+//         extension,
+//     )))
+// }
 
-struct ColoredText(egui::text::LayoutJob);
+// struct ColoredText(egui::text::LayoutJob);
 
-impl ColoredText {
-    pub fn ui(&self, ui: &mut egui::Ui) {
-        let mut job = self.0.clone();
-        job.wrap.max_width = ui.available_width();
-        let galley = ui.fonts(|f| f.layout_job(job));
-        ui.add(egui::Label::new(galley).selectable(true));
-    }
-}
+// impl ColoredText {
+//     pub fn ui(&self, ui: &mut egui::Ui) {
+//         let mut job = self.0.clone();
+//         job.wrap.max_width = ui.available_width();
+//         let galley = ui.fonts(|f| f.layout_job(job));
+//         ui.add(egui::Label::new(galley).selectable(true));
+//     }
+// }
